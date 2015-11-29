994026c48cc5c6d3cf267e81757eb82e81848298
Ganesh NJ <ganeshnj@outlook.com>
2015-11-29 23:55:15 +0100

    Added: AbstractView Added: InterpreterView

---
+++ java_cup.samples/src/calculator/InterpreterView.java
@@ -0,0 +1,44 @@
+package calculator;
+
+import java.awt.*;
+import java.io.FileReader;
+
+import org.apache.batik.swing.*;
+import org.apache.batik.svggen.*;
+import org.w3c.dom.*;
+import java_cup.runtime.ComplexSymbolFactory;
+
+public class InterpreterView extends AbstractView {
+	
+	public InterpreterView() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+	@Override
+	protected void onPaint(SVGGraphics2D g) {
+		// TODO Auto-generated method stub
+		try {    	
+	    	ComplexSymbolFactory csf = new ComplexSymbolFactory ();
+	    	Lexer l = new Lexer(new FileReader(this.getFilename()));
+	    	l.setSymbolFactory(csf);
+	    	Parser p = new Parser(l, csf);
+	    	p.setContainer(new MyContainer(getSvgGraphics()));
+	    	p.parse();
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    }
+		
+		getSvgGraphics().setSVGCanvasSize(new Dimension(640, 480));
+		
+		Element root = getSvgDocument().getDocumentElement();
+		getSvgGraphics().getRoot(root);
+		
+		JSVGCanvas canvas = new JSVGCanvas();
+	    this.getContentPane().add(canvas);
+	    canvas.setSVGDocument(getSvgDocument());
+	    this.pack();
+	    this.setVisible(true);
+	}
+
+}
